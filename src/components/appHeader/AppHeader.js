3f17596d0eab2eb69f91f0e8f25ed6be69e5a6b1
Ali Ghali <alifriends@live.com>
2020-05-16 09:52:30 +0200

    refactror(components): use custom hooks to get the weather

--- src/components/appHeader/AppHeader.js
+++ src/components/appHeader/AppHeader.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -6,6 +6,7 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MonetizationOnIcon from '@material-ui/icons/MonetizationOn';
+import useWeather from './useWeather';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -18,28 +19,16 @@ const useStyles = makeStyles((theme) => ({
     flexGrow: 1,
     textAlign: 'left',
   },
+  weather: {
+    display: 'flex',
+    alignItems: 'center',
+  },
 }));
 
-export default function AppHeader() {
+export default function AppHeader({ weatherApi, title }) {
   AppHeader.testId = 'app-header-test';
   const classes = useStyles();
-  const [weather, setWeather] = useState({});
-  useEffect(() => {
-    try {
-      fetch('http://openweathermap.org/data/2.5/weather?q=London,uk&appid=439d4b804bc8187953eb36d2a8c26a02')
-        .then((res) => res.json()).then((data) => {
-          if (data.name) {
-            setWeather({
-              city: data.name,
-              tempreature: Math.round(data.main.temp),
-              icon: data.weather[0].icon,
-            });
-          }
-        });
-    } catch (error) {
-      console.log('there is an error getting the weather infos');
-    }
-  }, []);
+  const weather = useWeather(weatherApi);
 
   return (
     <div className={classes.root} data-testid={AppHeader.testId}>
@@ -48,22 +37,24 @@ export default function AppHeader() {
           <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
             <MonetizationOnIcon fontSize="large" />
           </IconButton>
-          <Typography variant="h5" className={classes.title}>
-            Offer matcher
-          </Typography>
-          <Button color="inherit" href="https://openweathermap.org/city/2643743">
-            <span>
-              {' '}
-              {weather.city}
-            </span>
-          </Button>
-          {!!weather.icon && <img src={`http://openweathermap.org/img/wn/${weather.icon}.png`} alt="weather-icon" />}
-          <span>
-            {' '}
-            {weather.tempreature}
-            {' '}
-            °C
-          </span>
+          <Typography variant="h5" className={classes.title}>{title}</Typography>
+          {weather ? (
+            <div className={classes.weather}>
+              <Button color="inherit" href="https://openweathermap.org/city/2643743">
+                <span>
+                  {' '}
+                  {weather.city}
+                </span>
+              </Button>
+              {!!weather.icon && <img src={`http://openweathermap.org/img/wn/${weather.icon}.png`} alt="weather-icon" />}
+              <span>
+                {' '}
+                {weather.tempreature}
+                {' '}
+                °C
+              </span>
+            </div>
+          ) : <span>Loading ...</span>}
         </Toolbar>
       </AppBar>
     </div>
